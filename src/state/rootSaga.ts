4022d1691cbc053a5ebefd129c78cb5063d30571
Miroslav I <styylzeh@gmail.com>
2022-05-31 18:01:56 +0300

    feat: created quizAction and added to rootSaga

--- src/state/rootSaga.ts
+++ src/state/rootSaga.ts
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import { productsSagas } from "./products/ProductsSagas";
+import { quizQuestionsSagas } from "./quiz/QuizSaga";
 
 export default function* rootSaga(): Generator {
-  yield all([productsSagas()]);
+  yield all([productsSagas(), quizQuestionsSagas()]);
 }
