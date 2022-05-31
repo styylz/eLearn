16e07600e5dd8456558d4c48e254fd0d249ad76a
Miroslav I <styylzeh@gmail.com>
2022-05-31 18:33:35 +0300

    feat: created emailSlice

---
+++ src/state/slice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Statuses } from "typings/generalTypes";
+import { postEmail } from "./thunks";
+
+interface EmailState {
+  email: string;
+  status: Statuses;
+}
+
+const initialState: EmailState = {
+  email: "",
+  status: "idle",
+};
+
+const emailSlice = createSlice({
+  name: "email",
+  initialState,
+  reducers: {
+    setEmail: (state, action: PayloadAction<EmailState["email"]>) => {
+      state.email = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(postEmail.pending, (state, _) => {
+      state.status = "loading";
+    });
+    builder.addCase(postEmail.rejected, (state, _) => {
+      state.status = "error";
+    });
+    builder.addCase(postEmail.fulfilled, (state, _) => {
+      state.status = "success";
+    });
+  },
+});
+
+export const { setEmail } = emailSlice.actions;
+export default emailSlice;
