d65b6619b2a670b1b70f12a05021b496bae59935
Miroslav I <styylzeh@gmail.com>
2022-05-31 17:14:14 +0300

    fix: fixed boilerPlate

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import React, { StrictMode } from "react";
-import { Provider } from "react-redux";
-import { store } from "state/store";
 import { createGlobalStyle, ThemeProvider } from "styled-components/macro";
 import { theme } from "styles/theme";
 
@@ -20,11 +18,9 @@ html {
 
 export const wrapRootElement = ({ element }: any) => (
   <StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        {element}
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {element}
+    </ThemeProvider>
   </StrictMode>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
-import React, { useEffect } from "react";
-import { fetchProductsAction } from "state/products/ProductsActions";
-import { useAppDispatch } from "state/store";
+import React from "react";
 import Landing from "./landing";
 
 const Index = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(fetchProductsAction());
-  }, []);
-
   return <Landing />;
 };
 
