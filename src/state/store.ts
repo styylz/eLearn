2f86be3772b065a5599d36d3efc0c401742e0c6c
Miroslav I <styylzeh@gmail.com>
2022-05-31 18:03:21 +0300

    feat: added questionsReducer to the store

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,18 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { AnyAction, CombinedState, combineReducers } from "redux";
 import { useDispatch } from "react-redux";
 import createSagaMiddleware from "@redux-saga/core";
-import { ProductsData } from "./types";
+import { ProductsData, quizQuestionsData } from "./types";
 import productsSlice from "./products/ProductsSlice";
 import rootSaga from "./rootSaga";
+import quizQuestionsSlice from "./quiz/QuizSlice";
 
 export interface RootState {
   products: ProductsData;
+  questions: quizQuestionsData;
 }
 
 const sagaMiddleware = createSagaMiddleware();
 
 const combinedReducer = combineReducers<CombinedState<RootState>>({
   products: productsSlice.reducer,
+  questions: quizQuestionsSlice.reducer,
 });
 
 export const rootReducer = (state: any, action: AnyAction) =>
