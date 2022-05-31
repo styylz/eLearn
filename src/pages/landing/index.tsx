9f93ef6524594306b676f37e822b882a70e669a3
Miroslav I <styylzeh@gmail.com>
2022-05-31 17:18:19 +0300

    feat: created Hero section

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,10 +1,12 @@
 import { Box, Navbar } from "components";
 import React from "react";
+import { Hero } from "./sections";
 
 const Landing: React.FC = () => {
   return (
     <Box position="relative">
       <Navbar />
+      <Hero />
     </Box>
   );
 };
