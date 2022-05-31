d2adfa2d9e5dae3d2c587c8b5123a344e9e8672e
Miroslav I <styylzeh@gmail.com>
2022-05-31 18:09:37 +0300

    feat: created quiz page layout

---
+++ src/pages/quiz/index.tsx
@@ -0,0 +1,72 @@
+import {
+  Container,
+  FlexWrapper,
+  GreenButton,
+  Typography,
+  Box,
+  SectionWrapper,
+} from "components";
+import { navigate } from "gatsby";
+import React, { useEffect, useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
+import { useSelector } from "react-redux";
+import { selectProducts, selectQuizQuestions } from "state/selectors";
+//   import { ProgressBar, QuestionSlot } from "./elements";
+import { Score } from "typings/types";
+import { calcMaxCategoryValue } from "utils/calculate";
+import { useAppDispatch } from "state/store";
+import { Product } from "state/types";
+import { fetchQuizQuestionsAction } from "state/quiz/QuizActions";
+import { setFreeCourse } from "state/products/ProductsSlice";
+
+const Quiz: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [showScore, setShowScore] = useState<boolean>(false);
+  const [mostPopularCategory, setMostPopularCategory] = useState<string>("");
+  const [progressBarValue, setProgressBarValue] = useState(0);
+  const [score, setScore] = useState<Score>({
+    design: 0,
+    uiUx: 0,
+    IT: 0,
+  });
+
+  const dispatch = useAppDispatch();
+  const products = useSelector(selectProducts);
+  const questions = useSelector(selectQuizQuestions);
+
+  return (
+    <>
+      {loading ? (
+        <Box margin="auto" position="absolute" top="50%" left="50%">
+          <ThreeDots color="#00BFFF" height={80} width={80} />
+        </Box>
+      ) : (
+        <>
+          <SectionWrapper>
+            <Container>
+              <FlexWrapper flexDirection="column" alignItems="center">
+                <Typography type="h1" marginBottom="s32" textAlign="center">
+                  {questions[currentQuestion]?.questionText}
+                </Typography>
+                <FlexWrapper flexDirection="column" alignItems="center">
+                  {showScore ? (
+                    <Box marginX="auto" marginTop="s24">
+                      <GreenButton>Submit</GreenButton>
+                    </Box>
+                  ) : (
+                    ""
+                  )}
+                </FlexWrapper>
+              </FlexWrapper>
+
+              <Typography type="h3">{mostPopularCategory}</Typography>
+            </Container>
+          </SectionWrapper>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Quiz;
--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -1,3 +1,5 @@
 import { AppState } from "./types";
 
 export const selectProducts = (state: AppState) => state.products.products;
+export const selectQuizQuestions = (state: AppState) =>
+  state.questions.questions;
