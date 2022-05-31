3af4a0bba445af375419966b932c51112efd9301
Miroslav I <styylzeh@gmail.com>
2022-05-31 19:09:57 +0300

    feat: created data driven plans

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Banner, Plan, PlanHeader } from "./sections";
+import { useAppDispatch } from "state/store";
+import { selectPlans } from "state/selectors";
+import { fetchPlansAction } from "state/plans/PlansActions";
+import { useSelector } from "react-redux";
+import { PlanState } from "typings/types";
 
 const Checkout: React.FC = () => {
+  const [selectedPlan, setSelectedPlan] = useState<PlanState>({
+    id: "1",
+    title: "",
+    price: null as unknown as number,
+    oldPrice: null as unknown as number,
+    currPrice: null as unknown as number,
+    description: "",
+    selected: true,
+  });
+
+  const dispatch = useAppDispatch();
+  const plans = useSelector(selectPlans);
+
+  const handlePlanClick = (id: string) => {
+    const plan = plans.find((plan) => plan.id === id);
+    setSelectedPlan({
+      ...selectedPlan,
+      ...plan,
+      selected: !selectedPlan.selected,
+    });
+  };
+
+  useEffect(() => {
+    dispatch(fetchPlansAction());
+  }, []);
+
+  console.log({ plans });
+
   return (
     <>
       <Banner />
       <PlanHeader />
-      <Plan />
+      <Plan
+        plans={plans}
+        handlePlanClick={handlePlanClick}
+        selectedPlan={selectedPlan}
+      />
     </>
   );
 };
--- src/state/rootSaga.ts
+++ src/state/rootSaga.ts
@@ -1,7 +1,8 @@
 import { all } from "redux-saga/effects";
+import { plansSagas } from "./plans/PlansSagas";
 import { productsSagas } from "./products/ProductsSagas";
 import { quizQuestionsSagas } from "./quiz/QuizSaga";
 
 export default function* rootSaga(): Generator {
-  yield all([productsSagas(), quizQuestionsSagas()]);
+  yield all([productsSagas(), quizQuestionsSagas(), plansSagas()]);
 }
